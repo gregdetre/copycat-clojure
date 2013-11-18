b4f2c56bc98fd0a6e5b5698df045556fe8db6bde
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 13:30:40 -0500

    Updated project license metadata

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject copycat-clojure "0.1.0-SNAPSHOT"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "A port of Melanie Mitchell's Copycat model to Clojure"
   :url "https://github.com/gregdetre/copycat-clojure"
+  :license {:name "MIT License"
+            :url "https://github.com/gregdetre/copycat-clojure/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
 
