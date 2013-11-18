a744ed340c4f69d5dd2b766c7d4bf1f219d56fd2
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 15:54:12 -0500

    Set up formulas-test

--- src/copycat_clojure/formulas.clj
+++ src/copycat_clojure/formulas.clj
@@ -1,20 +1,21 @@
 (ns copycat-clojure.formulas
   )
-;; (in-ns 'copycat-clojure.core)
 
-(defn blah [a b] (+ a b))
+(declare weighted-average)
+(declare fake-reciprocal)
 
-;; (defn update-temperature [ws rule & rule-weakness]
-;;   (let [rule (:rule ws)
-;;         clamp-temperature (:clamp-temperature ws)
-;;         total-unhappiness (:total-strength ws)]
-;;     ;; inverted the if-logic
-;;     (if clamp-temperature
-;;       [:temperature (weighted-average [[total-unhappiness 8]
-;;                                        [rule-weakness .2]])]
-;;       [:rule-weakness (if rule
-;;                         (fake-reciprocal (:total-strength rule))
-;;                         100)])))
+
+(defn update-temperature [ws rule & rule-weakness]
+  (let [rule (:rule ws)
+        clamp-temperature (:clamp-temperature ws)
+        total-unhappiness (:total-strength ws)]
+    ;; inverted the if-logic
+    (if clamp-temperature
+      [:temperature (weighted-average [[total-unhappiness 0.8]
+                                       [rule-weakness 0.2]])]
+      [:rule-weakness (if rule
+                        (fake-reciprocal (:total-strength rule))
+                        100)])))
 
 
 ;; (defun get-answer-temperature-threshold-distribution (& bond-density)
--- test/copycat_clojure/formulas_test.clj
+++ test/copycat_clojure/formulas_test.clj
@@ -1,7 +1,8 @@
-;; (ns copycat-clojure.test.formulas-test
 (ns copycat-clojure.formulas-test
   (:use [clojure.test]
         [copycat-clojure.formulas]))
-;; (in-ns 'copycat-clojure.core-test)
 
+(deftest update-temperature-test
+  (testing
+   (is false)))
 
