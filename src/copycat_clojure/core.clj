089ac0d164acce233938a5bfc35a1ffa51dbe3a9
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 09:29:13 -0500

    Adding default Lein scaffolding

---
+++ src/copycat_clojure/core.clj
@@ -0,0 +1,15 @@
+(ns copycat-clojure.core
+  )
+
+;; call this in a helper .clj
+;; (in-ns copycat-clojure.core)
+
+
+(load "my_utils")
+(load "run")
+;; (load "lucid-util")
+;; (load "lucid-util")
+;; (load "workspace-structures")
+
+
+
