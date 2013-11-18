afadea6ca55b97d4e5857dbc4260bbbff13ce5e6
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 13:30:27 -0500

    Updated project description & url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject copycat-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A port of Melanie Mitchell's Copycat model to Clojure"
+  :url "https://github.com/gregdetre/copycat-clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]])
 
