f21148b7721e7c0f23071a610b09c48ccfd09fed
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 15:49:24 -0500

    Cleaning up my-utils assocs-in2

--- src/copycat_clojure/my_utils.clj
+++ src/copycat_clojure/my_utils.clj
@@ -1,21 +1,6 @@
 (ns copycat-clojure.my-utils
   )
 
-;; (in-ns 'copycat-clojure.core)
-
-;; (defn assocs-in2 [d & args]
-;;   ;; (apply -> d (for [[k v] args] (list assoc-in k v))))
-;;   (apply (comp (reverse (for [[k v] args] (list assoc-in k v)))
-;; (let [args [[:cr] [:c1 :c2]]
-;;       ]
-;;   ;; (apply (comp (reverse
-;;                 (for [k-and-v args] [(first k-and-v) (second k-and-v)]));; (list assoc-in k v)))
-;;   ;; ) {}))
-
-;; bleurgh. none of these work because you "can't take value of a macro"
-;; (apply -> d (for [[k v] args] (list assoc-in k v)))
-;; (apply -> d (map #(apply list assoc-in %) args))
-;; (apply -> d (map (fn [[k v]] (list assoc-in k v)) args))
 (defn assocs-in [d & args]
   "Like assoc-in, but variadic, so you can feed in multiple pairs "
   (if args
@@ -27,6 +12,4 @@
           moreargs (drop 2 args)]
       (apply assocs-in (assoc-in d keypath val) moreargs))
     d))
-
-
 
--- src/copycat_clojure/obsolete/my-utils.clj
+++ src/copycat_clojure/obsolete/my-utils.clj
@@ -12,3 +12,17 @@
 ;;               :c (+ :a 30)
 ;;               })
 
+
+;; (defn assocs-in2 [d & args]
+;;   ;; (apply -> d (for [[k v] args] (list assoc-in k v))))
+;;   (apply (comp (reverse (for [[k v] args] (list assoc-in k v)))
+;; (let [args [[:cr] [:c1 :c2]]
+;;       ]
+;;   ;; (apply (comp (reverse
+;;                 (for [k-and-v args] [(first k-and-v) (second k-and-v)]));; (list assoc-in k v)))
+;;   ;; ) {}))
+
+;; bleurgh. none of these work because you "can't take value of a macro"
+;; (apply -> d (for [[k v] args] (list assoc-in k v)))
+;; (apply -> d (map #(apply list assoc-in %) args))
+;; (apply -> d (map (fn [[k v]] (list assoc-in k v)) args))
