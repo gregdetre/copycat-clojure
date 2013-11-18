c0a10e41591152bda06be91cc3c61e348ca419f8
Greg Detre <greg@gregdetre.co.uk>
2013-11-18 14:18:26 -0500

    Commented core imports, so tests run

--- src/copycat_clojure/core.clj
+++ src/copycat_clojure/core.clj
@@ -1,15 +1,26 @@
 (ns copycat-clojure.core
   )
 
+;; e.g.
+;; (ns examples.ns
+;;   (:refer-clojure :exclude [next replace remove])
+;;   (:require (clojure [string :as string]
+;;                      [set :as set]) [clojure.java.shell :as sh])
+;;   (:use (clojure zip xml))
+;;   (:import java.util.Date
+;;            java.text.SimpleDateFormat (java.util.concurrent Executors
+;;                                                             LinkedBlockingQueue)))
+
+
 ;; call this in a helper .clj
 ;; (in-ns copycat-clojure.core)
 
 
-(load "my_utils")
-(load "run")
-;; (load "lucid-util")
-;; (load "lucid-util")
-;; (load "workspace-structures")
+;; (load "my_utils")
+;; (load "run")
+;; ;; (load "lucid-util")
+;; ;; (load "lucid-util")
+;; ;; (load "workspace-structures")
 
 
 
