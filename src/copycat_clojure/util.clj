898c6df73c5e52e6f569047a1788b4cde8857822
Greg Detre <greg@gregdetre.co.uk>
2013-11-19 20:59:46 -0500

    Added temperature-adjusted-probability

--- src/copycat_clojure/util.clj
+++ src/copycat_clojure/util.clj
@@ -107,3 +107,34 @@ an order of magnitude, set TOL to 10."
                (/ (- 100.0 temperature)
                   30.0))))
 
+
+(defn temperature-adjusted-probability [temperature x]
+  "Compared with this modified python code:
+    def temperatureAdjustedProbability(temperature, x):
+            if not x or x == 0.5 or not temperature:
+                    return x
+            if x < 0.5:
+                    return 1.0 - temperatureAdjustedProbability(temperature, 1.0 - x)
+            coldness = 100.0 - temperature
+            a = math.sqrt(coldness)
+            b = 10.0 - a
+            c = b / 100
+            d = c * (1.0 - (1.0 - x))  # aka c * x, but we're following the java
+            e = (1.0 - x) + d
+            f = 1.0 - e
+            return max(f, 0.5)
+"
+  (cond (or (zero? x)
+            (= x 0.5)
+            (zero? temperature)) x
+        (< x 0.5) (- 1.0 (temperature-adjusted-probability temperature (- 1 x)))
+        :else (let [coldness (- 100 temperature)
+                    a (Math/sqrt coldness)
+                    b (- 10 a)
+                    c (/ b 100)
+                    d (* c (- 1 (- 1 x)))
+                    e (+ (- 1 x) d)
+                    f (- 1 e)
+                    ]
+                (max f 0.5))))
+
--- test/copycat_clojure/util_test.clj
+++ test/copycat_clojure/util_test.clj
@@ -66,7 +66,7 @@
       ))
    )
 
-     
+
 (deftest test-weighted-average
   (testing "empty"
            (is (= (weighted-average nil)
@@ -102,3 +102,12 @@
            (is (=ish (temperature-adjusted-value 50 20) 659.0195889768269 1.000001))
            (is (=ish (temperature-adjusted-value 100 20) 4.47213595499958 1.000001))
            )
+
+
+(deftest test-temperature-adjusted-probability
+  (testing "based on running the python code with sample values"
+           (is (=ish (temperature-adjusted-probability 100 0) 0 1.000001))
+           (is (=ish (temperature-adjusted-probability 50 0) 0 1.000001))
+           (is (=ish (temperature-adjusted-probability 50 20) 19.414213562373096 1.000001))
+           (is (=ish (temperature-adjusted-probability 50 0.1) 0.12636038969321073 1.000001))
+           ))
